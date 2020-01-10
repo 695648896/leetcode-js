0c185c1d8a545725f15c8acd1bbef05e46d5d897
13330079275 <695648896@qq.com>
2020-01-10 14:44:39 +0800

    维护了一个新数组

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -9,26 +9,25 @@
  * @param {string} s
  * @return {number}
  */
+
 var lengthOfLongestSubstring = function(s) {
     if(s.length === 0 || s.length === 1){
         return s.length
     }
-    let longestNum=1
-    let arr = s.split('')
-    for(let i = 0; i < arr.length; i++){
-        let tempArr = [arr[i]]
-        for(let j = i + 1; j < arr.length; j++){
-            if(tempArr.includes(arr[j])){
-                let tempIndex = tempArr.indexOf(arr[j])
-                tempArr.splice(0,tempIndex + 1)
-                tempArr.push(arr[j])
-            }else{
-                tempArr.push(arr[j])
-                longestNum = tempArr.length > longestNum ? tempArr.length: longestNum
-            }
+    let arr = [], num = 1
+    for(let val of s){
+        if(arr.includes(val)){
+            let index = arr.indexOf(val)
+            arr.splice(0, index + 1)
         }
+        arr.push(val)
+        num = arr.length > num ? arr.length : num
     }
-    return longestNum
+    return num
 };
+// Accepted
+// 987/987 cases passed (100 ms)
+// Your runtime beats 68.16 % of javascript submissions
+// Your memory usage beats 87.06 % of javascript submissions (37.7 MB)
 // @lc code=end
 
