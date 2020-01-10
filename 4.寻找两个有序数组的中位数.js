61f23da285d7309763bed59e7fbd454e3aae188b
13330079275 <695648896@qq.com>
2020-01-10 15:47:18 +0800

    4

---
+++ 4.寻找两个有序数组的中位数.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=javascript
+ *
+ * [4] 寻找两个有序数组的中位数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let nums3 = nums1.concat(nums2)
+    nums3.sort((a,b)=> a-b)
+    if(nums3.length % 2 === 0){
+        return (nums3[nums3.length/2] + nums3[nums3.length/2 -1])/2
+    }else{
+        return nums3[(nums3.length - 1)/2]
+    }
+};
+// @lc code=end
+
